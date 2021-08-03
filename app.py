16d5f2e4dfcb20dd9bd8369206b6b21b40805d90
RUDRANSH AGNIHOTRI <59211507+rudransh2004@users.noreply.github.com>
2021-08-03 22:11:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+import datetime
+# creating a Flask app
+app = Flask(__name__)
+ct = datetime.datetime.now()
+ct = ct.timestamp()  
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = ct
+        return jsonify({'value': data})
+
+if __name__ == '__main__':
+      
+    app.run(debug = False)        
